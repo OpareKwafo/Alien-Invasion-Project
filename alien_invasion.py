fc36ba3c41e8b8679a7a5513a1ad73d2f2fee136
Opare Kwafo <oparekwafo@gmail.com>
2026-04-16 13:31:36 -0600

    adding helper functions:making it modular

--- alien_invasion.py
+++ alien_invasion.py
@@ -24,18 +24,26 @@ class AlienInvasion:
     def run_game(self):
         """start the main loop for the game"""
         while True:
-            #watch for keyboard and mouse events
+            self._check_events()
+            self._update_screen()
+            self.clock.tick(60)
+            
+
+    def _check_events(self):
+            """Respond to keypresses and mouse events"""
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-            # Redraw the screen during each pass through the loop
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
+    def _update_screen(self):
+          """Update images on the screen, and flip to the new screen"""
+          self.screen.fill(self.settings.bg_color)
+          self.ship.blitme()
 
-            # Make the most recently drawn screen visible
-            pygame.display.flip()
-            self.clock.tick(60)
+          # Make the most recently drawn screen visible
+          pygame.display.flip()
+         
+        
 
 
 if __name__ == '__main__':
