9df047d971d70783d21a627e0e6df86466b13e77
Opare Kwafo <oparekwafo@gmail.com>
2026-04-16 15:45:24 -0600

    feature:shooting bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from ship import Ship
+from bullet import Bullet
 
 from settings import Settings
 
@@ -15,10 +16,12 @@ class AlienInvasion:
         self.settings = Settings()
 
         #set the screen
-        self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_height))
+        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invastion")
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
 
     def run_game(self):
@@ -26,6 +29,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
+            self.bullets.update()
             self._update_screen()
             self.clock.tick(60)
             
@@ -48,6 +52,8 @@ class AlienInvasion:
             self.ship.moving_left = True
          elif event.key == pygame.K_q:
              sys.exit()
+         elif event.key == pygame.K_SPACE:
+             self._fire_bullet()
 
     def _check_keyup_events(self, event):
           """Respond to key releases"""
@@ -56,9 +62,17 @@ class AlienInvasion:
           elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
 
+    def _fire_bullet(self):
+        """Create a new bullet and add it to the bullet's group"""
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
+
+
     def _update_screen(self):
           """Update images on the screen, and flip to the new screen"""
           self.screen.fill(self.settings.bg_color)
+          for bullet in self.bullets.sprites():
+              bullet.draw_bullet()
           self.ship.blitme()
 
           # Make the most recently drawn screen visible
